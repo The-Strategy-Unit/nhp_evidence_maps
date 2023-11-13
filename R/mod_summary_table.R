8c7d4054065fd887fff4fb2ea4a4c35b4bd1a7c9
Mohammed Ozayr (MLCSU) <Ozayr.Mohammed@mlcsu.nhs.uk>
2023-11-13 01:25:26 +0000

    refactored code

--- R/mod_summary_table.R
+++ R/mod_summary_table.R
@@ -35,12 +35,13 @@ mod_summary_table_ui <- function(id) {
                                    ),
                        selected = "",
                        multiple = T),
-      shiny::fluidRow(
-        column(width = 8, DT::DTOutput(ns("summary"))),
-        column(width = 4, shiny::plotOutput(ns("waffle")))
-      ),
     shiny::verbatimTextOutput(ns("debug")),
     shiny::verbatimTextOutput(ns("debugVarSelect")),
+    shiny::verbatimTextOutput(ns("inputValsDebug")),
+      shiny::fluidRow(
+        column(width = 8, DT::DTOutput(ns("evidenceMap"))),
+        column(width = 4, shiny::plotOutput(ns("waffle")))
+      ),
     DT::DTOutput(ns("debugData")),
     DT::DTOutput(ns("selectedTable"))
     )
@@ -52,21 +53,24 @@ mod_summary_table_ui <- function(id) {
 mod_summary_table_server <- function(id) {
   shiny::moduleServer(id, function(input, output, session) {
     ns <- session$ns
-
-    selectedYear <- reactive(input$yearSelect)
     
-    
-    output$debug <- shiny::renderPrint(selectedYear())
-    
+    output$debug <- shiny::renderPrint(reactiveValuesToList(selectedCell))
+    output$inputValsDebug <- shiny::renderPrint(selectedVars())
+# setup ----    
     evidence_map_skeleton <- data |>
       dplyr::select(Mechanism, `Type of evidence`) |>
       dplyr::group_by(Mechanism, `Type of evidence`) |>
       dplyr::summarise(count = dplyr::n()) |>
       dplyr::select(-count)
-
     
+    selectedYear <- reactive(input$yearSelect)
+    selectedVars <- reactive(input$varSelect)
+    
+    selectedCell <- reactiveValues(row = 1,
+                                   col = 1)
+ 
+# dataframe to be used in tab ----       
     summary_tab_data <- reactive({
-      shiny::req(selectedYear())
       data |>
         dplyr::filter(`Publication year` == selectedYear() | selectedYear() == "All Years") |>
         dplyr::select(tidyselect::any_of(c("Mechanism",
@@ -76,16 +80,28 @@ mod_summary_table_server <- function(id) {
                                            "Link",
                                            input$varSelect)))|>
         dplyr::distinct()
-    })
+      
+      })
+
+    
+# flush cell selection, potential bug ----        
+    # observe({
+    #   shiny::req(selectedVars(), selectedYear())
+    #   # flush selected cell index
+    #   selectedCell$row <- NULL
+    #   selectedCell$col <- NULL
+    # })
     
     
+    
+    
+# evidence map ----    
     evidence_map_data <- reactive({
-      shiny::req(summary_tab_data())
-      skeleton |> dplyr::left_join(
+      evidence_map_skeleton |> dplyr::left_join(
         summary_tab_data() |>
-        dplyr::select(Mechanism, `Type of evidence`) |>
-        dplyr::group_by(Mechanism, `Type of evidence`) |>
-        dplyr::summarise(count = dplyr::n())) |>
+          dplyr::select(Mechanism, `Type of evidence`) |>
+          dplyr::group_by(Mechanism, `Type of evidence`) |>
+          dplyr::summarise(count = dplyr::n())) |>
         tidyr::pivot_wider(
           names_from = `Type of evidence`,
           values_from = count) |> 
@@ -93,87 +109,82 @@ mod_summary_table_server <- function(id) {
         dplyr::mutate(id = dplyr::row_number())
     })
     
+    output$evidenceMap <- DT::renderDT(
+        evidence_map_data() |>
+          dplyr::select(-id),
+        options = list(
+          dom = "t",
+          ordering = F
+        ),
+        selection = list(
+          mode = "single",
+          target = "cell"
+        ),
+        rownames = F
+      )
     
-    waffle_data <- reactive({
-      shiny::req(summary_tab_data())
-      summary_tab_data() |>
-        dplyr::select(Mechanism, `Type of evidence`)
-    })
-
-    output$summary <- DT::renderDT(
-      evidence_map_data() |>
-        dplyr::select(-id),
-      options = list(
-        dom = "t",
-        ordering = F
-      ),
-      selection = list(
-        mode = "single",
-        target = "cell"
-      ),
-      rownames = F
-    )
-
     
-    selected_tab_data <- reactive({
-      summary_tab_data()
-    })
-
+# selected cell ----
     shiny::observe({
-      shiny::req(evidence_map_data())
-      index <- shiny::req(input$summary_cells_selected)
+      index <- shiny::req(input$evidenceMap_cells_selected)
 
       row <- index[[1]]
       col <- index[[2]] + 1
 
-      row_selected <- evidence_map_data()$Mechanism[row]
-      col_selected <- names(evidence_map_data()[col])
+      selectedCell$row <- evidence_map_data()$Mechanism[row]
+      selectedCell$col <- names(evidence_map_data()[col])
 
-      summary_selected <- summary_tab_data() |> 
-        dplyr::filter(
-          Mechanism == row_selected,
-          `Type of evidence` == col_selected
-          ) |> 
-        dplyr::select(-Mechanism, -`Type of evidence`)
 
-      output$selectedTable <- DT::renderDT(summary_selected,
-                                           options = list(
-                                             dom = "t",
-                                             ordering = F
-                                             ),
-                                           rownames = F,
-                                           escape = F,
-                                           selection = "none"
-                                           )
-      
-      output$waffle <- shiny::renderPlot({
-        
-        filtered_waffle_data <- waffle_data()|>
-          dplyr::filter(Mechanism == row_selected) |> 
-          ggwaffle::waffle_iron(
-            ggwaffle::aes_d(group = 'Type of evidence')) |> 
-          dplyr::mutate(selected = ifelse(group == col_selected, T, F))
-        
-        shiny::req(filtered_waffle_data)
-        filtered_waffle_data |> 
-          ggplot2::ggplot(ggplot2::aes(x, y, fill = group))+
-          ggwaffle::geom_waffle()+
-          ggwaffle::geom_waffle(data = filtered_waffle_data |> 
-                                  dplyr::filter(selected == T),
-                                colour = 'blue',
-                                show.legend = F)+
-          ggplot2::coord_equal()+
-          viridis::scale_fill_viridis(discrete = T)+
-          ggwaffle::theme_waffle()+
-          ggplot2::theme(axis.title.x = ggplot2::element_blank(),
-                         axis.title.y = ggplot2::element_blank(),
-                         legend.position = 'top',
-                         legend.title = ggplot2::element_blank())+
-          ggplot2::guides(fill = ggplot2::guide_legend(nrow=2, byrow=T))
-      })
-      
     })
-  })
+    
+    
+# waffle ----
+    
+    
+    output$waffle <- shiny::renderPlot({
+      
+      filtered_waffle_data <- summary_tab_data() |>
+        dplyr::select(Mechanism, `Type of evidence`) |>
+        dplyr::filter(Mechanism == selectedCell$row) |> 
+        ggwaffle::waffle_iron(
+          ggwaffle::aes_d(group = 'Type of evidence')) |> 
+        dplyr::mutate(selected = ifelse(group == selectedCell$col, T, F))
+      
+      shiny::req(filtered_waffle_data)
+      filtered_waffle_data |> 
+        ggplot2::ggplot(ggplot2::aes(x, y, fill = group))+
+        ggwaffle::geom_waffle()+
+        ggwaffle::geom_waffle(data = filtered_waffle_data |> 
+                                dplyr::filter(selected == T),
+                              colour = 'blue',
+                              show.legend = F)+
+        ggplot2::coord_equal()+
+        viridis::scale_fill_viridis(discrete = T)+
+        ggwaffle::theme_waffle()+
+        ggplot2::theme(axis.title.x = ggplot2::element_blank(),
+                       axis.title.y = ggplot2::element_blank(),
+                       legend.position = 'top',
+                       legend.title = ggplot2::element_blank())+
+        ggplot2::guides(fill = ggplot2::guide_legend(nrow=2, byrow=T))
+    })
+
+    
+    output$selectedTable <- summary_tab_data() |>
+      dplyr::filter(
+        Mechanism == selectedCell$row,
+        `Type of evidence` == selectedCell$col
+        ) |> 
+      dplyr::select(-Mechanism, -`Type of evidence`) |> 
+      DT::renderDT(
+      options = list(
+        dom = "t",
+        ordering = F
+      ),
+      rownames = F,
+      escape = F,
+      selection = "none"
+      )
+})
 }
 
 ## To be copied in the UI
