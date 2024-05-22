bd2f8b87f24562fba671564f9f71886777fb77b3
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2024-05-22 11:09:21 +0100

    Add logos

--- R/mod_about.R
+++ R/mod_about.R
@@ -15,6 +15,13 @@ mod_about_ui <- function(id) {
       title = "About",
       width = 12,
       collapsible = FALSE,
+      tags$img(src = "www/nhp_logo.png", style = "height: 150px;"),
+      shiny::HTML("&nbsp;&nbsp;&nbsp;"),
+      tags$img(
+        src = "www/tsu_logo_black_screen_transparent.png",
+        style = "height: 150px;"
+      ),
+      shiny::HTML("<br/><br/>"),
       shiny::htmlOutput(ns("about_html"))
     )
   )
