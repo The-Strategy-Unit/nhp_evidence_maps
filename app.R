fc984c80fb085ee78d347f6ad0f6e862610efab3
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2024-10-15 09:58:59 +0100

    Add contributors, update imports, rename package

--- app.R
+++ app.R
@@ -4,4 +4,4 @@
 
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
-evidence_maps::run_app() # add parameters here (if any)
+nhp.evidence.maps::run_app() # add parameters here (if any)
